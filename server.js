b3c5bda4b40275997eebe3186fdc90f9b86531b4
hsinawa <55555736+hsinawa@users.noreply.github.com>
2021-11-23 19:21:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ let dbconnection = require('./auth')
 
 
 
-app.get("/" , (req,res)=>{
-    res.send("Welcome to Minor Project Balram")
-} )
+
 
 
 
@@ -24,10 +22,20 @@ app.use('/api/orders/' , orderroute )
 
 
 
+if(process.env.NODE_ENV==='production')
+
+{
+    app.use('/' , express.static('client/build') )
+    app.get('*' , (req,res)=>{
+        res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
+    } )
+}
 
 
-const port =  8080 ;
+const port = process.env.PORT ||  8080 ;
 
- app.listen( port , ()=>{
+var server = app.listen( port , ()=>{
     console.log('Server started of Ecommerce')
 } )
+
+
